736dea1bde1ccb696363156b775fe21b4ace43c1
soumen <soumenmaity433@.com>
2022-04-27 13:35:40 +0530

    change-2

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -6,7 +6,6 @@ const BlogModel = require("../models/blogModel")
 
 const createBlog = async function (req, res) {                                    // Second API
 
-<<<<<<< HEAD
     try {
         let data = req.body
         let savedData = await BlogModel.create(data)
@@ -17,11 +16,8 @@ const createBlog = async function (req, res) {                                  
         console.log(error)
         res.status(400).send({ status: false, msg: "connection failed" })
     }
-=======
-catch (error){
-    res.status(400).send({ status: false, msg: error.message})
-}
->>>>>>> 8a53d673de025e7fac75246da1004ed944cfa578
+
+    
 }
 
 const getBlog = async function (req, res) {
