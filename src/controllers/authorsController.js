4932a3f13b3adf6ad6307699f3636f74dfa09753
Mohammad Naseem <niceman1915@gmail.com>
2022-04-30 21:17:15 +0530

    start of final changes

--- src/controllers/authorsController.js
+++ src/controllers/authorsController.js
@@ -1,20 +1,16 @@
 
 const AuthorModel = require("../models/authorModel")
-function validateEmail(usremail) {
+function validateEmail(usermail) {
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailPattern.test(usremail);
+    return emailPattern.test(usermail);
 }
 
 const createAuthor = async function (req, res) {
 
     try {
         let data = req.body
+        if (!validateEmail(data.email)) return res.status(400).send({ status: false, msg: "plz enter valid email(like-aBcd123@gmail.com)" })
         if (Object.keys(data).length != 0) {
-            if (!data.fname) return res.status(400).send({ status: false, msg: "plz enter fname" })
-            if (!data.lname) return res.status(400).send({ status: false, msg: "plz enter lname" })
-            if (!data.email) return res.status(400).send({ status: false, msg: "plz enter email" })
-            if (!data.password) return res.status(400).send({ status: false, msg: "plz enter password" })
-            if (!validateEmail(data.email)) return res.status(400).send({ status: false, msg: "plz enter valid email(like-aBcd123@gmail.com)" })
             let a = await AuthorModel.find({ email: data.email })
             if (a.length != 0) return res.status(400).send({ status: false, msg: "email already used" })
             let savedData = await AuthorModel.create(data)
